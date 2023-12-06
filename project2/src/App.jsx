b8fa29b4f8bf1d77bb5e162089bf8a3121bdd3fc
Riley Meere <rime1945@colorado.edu>
2023-12-05 23:13:09 -0700

    fix/update API integration

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -4,13 +4,9 @@ import Cat from "./Cat.jsx";
 
 import CatOneF1 from "./assets/basic-cat-1.png";
 import CatOneF2 from "./assets/basic-cat-2.png";
-import CatOneAudio from "./assets/meow.wav"
-
-let CatOneText = "yeah";
 
 const CLIENT_ID = "252a84a890044ce4ab4ba5aef17913e1";
 const CLIENT_SECRET = "cddf795f75884c5ea515b37de823639a";
-const SAMPLE_PLAYLIST_ID = '37i9dQZF1DX9wa6XirBPv8'
 
 export default function App() {
   const [accessToken, setAccessToken] = useState("");
@@ -23,34 +19,20 @@ export default function App() {
       },
       body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
     }
-    fetch('https://acounts.spotify.com/api/token', authParameters)
+    fetch('https://accounts.spotify.com/api/token', authParameters)
     .then(result => result.json())
     .then(data => setAccessToken(data.access_token))
+    .catch((error) => {
+      console.log(error)
+    })
   }, [])
 
   return (
   <div>
   <h1>beats and cats !</h1>
-  <Cat imgSrc1={ CatOneF1 } imgSrc2 = { CatOneF2 } catAudio = { CatOneAudio } text = { CatOneText } />
+  <Cat imgSrc1={ CatOneF1 } imgSrc2 = { CatOneF2 } access = {accessToken} />
   </div>
   );
-  }
-
-  async function getSong() {
-    var playlistParameters = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
-      }
-    }
-    // choose which playlist to use
-    var playlistID = await fetch('https://api.spotify.com/v1/playlists/' + SAMPLE_PLAYLIST_ID, playlistParameters)
-      .then(response => response.json())
-      .then(data => console.log(data))
-
-
-
   }
 
   
--- project2/src/Cat.jsx
+++ project2/src/Cat.jsx
@@ -1,27 +1,49 @@
 import './Cat.css';
 import { useState } from 'react';
 
-const CLIENT_ID = "252a84a890044ce4ab4ba5aef17913e1";
-const CLIENT_SECRET = "cddf795f75884c5ea515b37de823639a";
+const SAMPLE_PLAYLIST_ID = '37i9dQZF1DX9wa6XirBPv8';
 
-export default function Cat({ imgSrc1 , imgSrc2, catAudio , text }) {
+
+export default function Cat({ imgSrc1 , imgSrc2 , access}) {
 
     const [ catImage , UpdateCatImage ] = useState(imgSrc1);
-
-    let audio = new Audio(catAudio);
-    audio.addEventListener("ended", CloseMouth);
 
     function Sing(){
         UpdateCatImage(imgSrc2);
         audio.play();
+        getSong();
     }
 
     function CloseMouth() {
         UpdateCatImage(imgSrc1);
     }
 
+    function getSong() {
+        var playlistParameters = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access
+          }
+        }
+        // choose which playlist to use
+        var playlistID = fetch('https://api.spotify.com/v1/playlists/' + SAMPLE_PLAYLIST_ID, playlistParameters)
+          .then(response => response.json())
+          .then(data => console.log(data.tracks.items))    
+    }
+
+    let audioPrev = "o";
+
+
+    let audio = new Audio(audioPrev);
+    audio.addEventListener("ended", CloseMouth);
+
     return (
-    <div><button onClick={ Sing } className="Cat"><img src={catImage}></img></button><p>{ text }</p></div>
+    <div><button onClick={ Sing } className="Cat"><img src={catImage}></img></button></div>
     );
+
+
 }
+
+
     
