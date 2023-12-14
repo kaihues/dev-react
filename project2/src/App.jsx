ab917d34dc47cb528560e5341c25a06221a6354f
Riley Meere <rime1945@colorado.edu>
2023-12-13 19:23:59 -0700

    making progress

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -27,34 +27,118 @@ const playlistID_4 = '37i9dQZF1DWZCkamcYMQkz'; // "Feel Good Jazz"
 const siteTitle = "Beats n' Cats";
 
 export default function App() {
-  const [accessToken, setAccessToken] = useState(""); 
+  const [accessToken, setToken] = useState(''); 
 
-  useEffect(() =>{
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [ playlist, setPlaylist ] = useState(null);
+  const [ catSong, UpdateCatSong ] = useState(null);
+  const [ trackNum, updateTrackNum ] = useState(0);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const client_id = CLIENT_ID;
+      const client_secret = CLIENT_SECRET;
+
+      const authOptions = {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'grant_type=client_credentials',
+      };
+
+      try {
+        const response = await fetch('https://accounts.spotify.com/api/token', authOptions);
+
+        if (response.ok) {
+          const data = await response.json();
+          const accessToken = data.access_token;
+          setToken(accessToken);
+          console.log(accessToken);
+          await fetchPlaylist(accessToken, playlistID_1);
+        } else {
+          console.error('Error fetching token:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching token:', error);
+      }
+    };
+
+    fetchToken();
+
+     
+  }, [playlistID_1]);
+
+  async function fetchPlaylist(accessToken) {
+    try {
+      const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID_1}`, {
+        headers: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+
+      if (response.ok) {
+        const playlistData = await response.json();
+        console.log('Playlist data:', playlistData);
+      } else {
+        console.error('Error fetching playlist:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching playlist:', error);
     }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-    .then(result => result.json())
-    .then(data => setAccessToken(data.access_token))
-    .then(console.log("successful request"))
-    .catch((error) => console.log(error))
-  }, []);
+  }
+
+    // var playlistParameters = {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer ' + access
+    //   }
+    // }
+    // load each playlist as state object
+    // fetch('https://api.spotify.com/v1/playlists/' + playlistID, playlistParameters)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //     setPlaylist(data.tracks.items);
+    //     let n = 0;
+    //     while (catSong == null) {
+    //       n = Math.floor(Math.random() * data.tracks.total);
+    //       let song = data.tracks.items[n].track.preview_url;
+    //       UpdateCatSong(song);
+    //     }
+    //     updateTrackNum(n);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.message);
+    //   });
+
+  // return (
+  // <div class = "FullApp">
+  //   <img class = "Title" src= { Title } />
+  //   <ul class = "CatList">
+  //     <li><Cat imgSrc1={ CatOneF1 } imgSrc2 = { CatOneF2 } audioSrc = { null } /></li>
+  //     <li><Cat imgSrc1={ CatTwoF1 } imgSrc2 = { CatTwoF2 } audioSrc = { null } /></li>
+  //     <li><Cat imgSrc1={ CatThreeF1 } imgSrc2 = { CatThreeF2 } audioSrc = { null } /></li>
+  //     <li><Cat imgSrc1={ CatFourF1 } imgSrc2 = { CatFourF2 } audioSrc = { null } /></li>
+  //   </ul>
+  // </div>
+  // );
 
   return (
-  <div class = "FullApp">
-    <img class = "Title" src= { Title } />
-    <ul class = "CatList">
-      <li><Cat imgSrc1={ CatOneF1 } imgSrc2 = { CatOneF2 } access = { accessToken } playlistID = { playlistID_1 } /></li>
-      <li><Cat imgSrc1={ CatTwoF1 } imgSrc2 = { CatTwoF2 } access = { accessToken } playlistID = { playlistID_2 } /></li>
-      <li><Cat imgSrc1={ CatThreeF1 } imgSrc2 = { CatThreeF2 } access = { accessToken } playlistID = { playlistID_3 } /></li>
-      <li><Cat imgSrc1={ CatFourF1 } imgSrc2 = { CatFourF2 } access = { accessToken } playlistID = { playlistID_4 } /></li>
-    </ul>
-  </div>
-  );
+    <div class = "FullApp">
+      <img class = "Title" src= { Title } />
+      <ul class = "CatList">
+        <li><Cat imgSrc1={ CatOneF1 } imgSrc2 = { CatOneF2 } audioSrc = { null } /></li>
+        <li><Cat imgSrc1={ CatTwoF1 } imgSrc2 = { CatTwoF2 } audioSrc = { null } /></li>
+        <li><Cat imgSrc1={ CatThreeF1 } imgSrc2 = { CatThreeF2 } audioSrc = { null } /></li>
+        <li><Cat imgSrc1={ CatFourF1 } imgSrc2 = { CatFourF2 } audioSrc = { null } /></li>
+      </ul>
+    </div>
+    );
   }
 
   
--- project2/src/Cat.jsx
+++ project2/src/Cat.jsx
@@ -1,44 +1,11 @@
 import './Cat.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-export default function Cat({ imgSrc1 , imgSrc2 , access , playlistID }) {
+export default function Cat({ imgSrc1 , imgSrc2 , audioSrc }) {
 
   const [ catImage , UpdateCatImage ] = useState(imgSrc1);
-  const [ catSong, UpdateCatSong ] = useState(null);
 
-  const [playlist, setPlaylist] = useState(null);
-  const [ trackNum, updateTrackNum ] = useState(0);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
-
-  useEffect(() => {
-    var playlistParameters = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + access
-      }
-    }
-    // load each playlist as state object
-    fetch('https://api.spotify.com/v1/playlists/' + playlistID, playlistParameters)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setPlaylist(data.tracks.items);
-        let n = 0;
-        while (catSong == null) {
-          n = Math.floor(Math.random() * data.tracks.total);
-          let song = data.tracks.items[n].track.preview_url;
-          UpdateCatSong(song);
-        }
-        updateTrackNum(n);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
-  let audio = new Audio(catSong);
+  let audio = new Audio(audioSrc);
     
     if (audio) {
         audio.addEventListener("ended", CloseMouth);
